3f4db0e1cb04f9f974c710f36980cc5b73b725aa
Tamiliplon <107620306+Tamiliplon@users.noreply.github.com>
2023-01-05 17:09:45 +0530

    Update harbortesting.sh

--- harbortesting.sh
+++ harbortesting.sh
@@ -1,2 +1,4 @@
+cd /home/iplon/repos_2
+
 docker pull harbour.iplon.co.in/apache/v1@sha256:1a48471031edcb899dfd7c05fb044b0594d814ccb36454a9178af5cd1464d29b
 docker run -itd --name harbortesting --network host apache/v1
