a75fa7d16eb3d2747aa6cce5a45101d1f58f9f29
tamilwelcome <93776743+tamilwelcome@users.noreply.github.com>
2023-01-06 11:19:30 +0530

    Update harbortesting.sh

--- harbortesting.sh
+++ harbortesting.sh
@@ -3,5 +3,5 @@ cd /home/iplon/repos_2
 
 sudo usermod -aG docker iplon
 
-docker pull nodered/node:latest
-docker run -itd --name noderedansibletest -p 1889:1880 nodered/node:latest
+docker pull nodered/node-red
+docker run -itd --name noderedansibletest -p 1889:1880 nodered/node-red
